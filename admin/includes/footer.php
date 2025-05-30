6b81bc932d640f10d83e6583d654fe033e92cf2b
iameffat <53122076+iameffat@users.noreply.github.com>
2025-05-30 10:26:34 +0600

    admin footer

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,5 +1,5 @@
 </main> <footer class="admin-footer">
-    &copy; <?php echo date("Y"); ?> দ্বীনিলাইফ কুইজ - এডমিন প্যানেল
+    &copy; <?php echo date("Y"); ?> DeeneLife Quiz | Powered by DeeneLife.Com
 </footer>
 
 <script src="<?php echo $current_admin_base_url; ?>../assets/js/bootstrap.bundle.min.js"></script>
