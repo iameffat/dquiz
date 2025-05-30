c252812bf8fcc821e3d5a6dfa0647af72d398c64
iameffat <53122076+iameffat@users.noreply.github.com>
2025-05-30 10:29:00 +0600

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 </main> <footer class="footer mt-auto py-3 bg-light">
     <div class="container text-center">
-        <span class="text-muted"><?php echo date("Y"); ?> DeeneLife Quiz | Powered by DeeneLife Team</span>
+        <span class="text-muted"><?php echo date("Y"); ?> Powered by DeeneLife Team</span>
     </div>
 </footer>
 
