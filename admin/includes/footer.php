cea9edff382c644d81cc1d10b80239882d6421b3
iameffat <53122076+iameffat@users.noreply.github.com>
2025-05-30 15:35:20 +0600

    change

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,5 +1,5 @@
 </main> <footer class="admin-footer">
-    &copy; <?php echo date("Y"); ?> Powered by DeeneLife Team
+    <?php echo date("Y"); ?> Powered by DeeneLife Team
 </footer>
 
 <script src="<?php echo $current_admin_base_url; ?>../assets/js/bootstrap.bundle.min.js"></script>
