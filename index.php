73295ebc96a861486904cbc25c0145547e6b0cbe
iameffat <53122076+iameffat@users.noreply.github.com>
2025-06-04 12:15:52 +0600

    add

--- index.php
+++ index.php
@@ -502,8 +502,7 @@ require_once 'includes/header.php';
                                     onclick="shareQuiz('<?php echo htmlspecialchars(addslashes($quiz['title']), ENT_QUOTES); ?>', '<?php echo htmlspecialchars($quiz_page_url_home, ENT_QUOTES); ?>', this)">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
                                     <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5"/>
-                                </svg> শেয়ার
-                            </button>
+                                </svg></button>
                         </div>
                         <?php if (!empty($additional_info_home)): ?>
                             <div class="additional-info-home"><?php echo $additional_info_home; ?></div>
