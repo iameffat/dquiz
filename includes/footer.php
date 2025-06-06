f7eee1cb68eb0cdc69f50fe4f606ccc70a8dd237
iameffat <53122076+iameffat@users.noreply.github.com>
2025-06-06 10:14:43 +0600

    add

--- includes/footer.php
+++ includes/footer.php
@@ -7,18 +7,5 @@
 <script src="<?php echo isset($base_url) ? $base_url : ''; ?>assets/js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo isset($base_url) ? $base_url : ''; ?>assets/js/theme-switcher.js"></script> <?php // ?>
 <script src="<?php echo isset($base_url) ? $base_url : ''; ?>assets/js/script.js"></script>
-<script>
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('<?php echo $base_url; ?>service-worker.js') // base_url ব্যবহার করুন
-      .then(registration => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch(err => {
-        console.log('ServiceWorker registration failed: ', err);
-      });
-  });
-}
-</script>
 </body>
 </html>
