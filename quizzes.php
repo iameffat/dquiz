c8561a5b07ef9e6ec0ffa328a82cc9cda3658094
iameffat <53122076+iameffat@users.noreply.github.com>
2025-06-07 02:36:38 +0600

    add

--- quizzes.php
+++ quizzes.php
@@ -242,7 +242,7 @@ require_once 'includes/header.php';
                                     <div class="action-buttons">
                                         <button class="btn btn-sm btn-outline-dark" onclick="shareQuiz('<?php echo htmlspecialchars(addslashes($quiz['title']), ENT_QUOTES); ?>', '<?php echo $quiz_page_url; ?>', this)">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16"><path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5"/></svg>
-                                            শেয়ার
+                                         
                                         </button>
                                         <button type="button" class="btn btn-sm btn-outline-secondary syllabus-btn" 
                                                 data-bs-toggle="modal" 
